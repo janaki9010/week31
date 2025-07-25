d39f483825be8f68e70c4f6381583e9be5c35bd1
vijji <vijayashree.cse@anurag.ac.in>
2025-07-25 14:34:25 +0530

    modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld {
     // Prints "Hello, World" to the terminal window.
     public static void main(String[] args)
     {
-        System.out.println("Hello, World Java 3");
+        System.out.println("Hello, World Java 5");
     }
 }
